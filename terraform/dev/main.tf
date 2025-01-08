6a22690bdf469518ae0c8207f48e70d599ea0528
fungame2270 <tomasvictal@gmail.com>
2025-01-08 21:16:03 +0000

    added s3 to dev terraform

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -8,6 +8,7 @@ output "cognito_user_pool_client_id" {
 
 output "cognito_user_pool_client_secret" {
   value = aws_cognito_user_pool_client.user_pool_client.client_secret
+  sensitive = true
 }
 
 output "cognito_hosted_domain" {
@@ -20,7 +21,7 @@ provider "aws" {
 
 # Cognito User Pool
 resource "aws_cognito_user_pool" "user_pool" {
-  name = "Nook_test"
+  name = "Nook_test_dev"
 
   # Configuration for user attributes
   schema {
@@ -51,7 +52,7 @@ resource "aws_cognito_user_pool" "user_pool" {
 
 # Cognito User Pool Client
 resource "aws_cognito_user_pool_client" "user_pool_client" {
-  name         = "NookClientTest"
+  name         = "NookClientTest_dev"
   user_pool_id = aws_cognito_user_pool.user_pool.id
 
   generate_secret = true
@@ -70,6 +71,87 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
 }
 
 resource "aws_cognito_user_pool_domain" "Nook-Domain" {
-  domain       = "nook-domain"  # This is the unique domain name you choose for your Cognito Hosted UI
+  domain       = "nook-domain-dev"  # This is the unique domain name you choose for your Cognito Hosted UI
   user_pool_id = aws_cognito_user_pool.user_pool.id  # Reference to your existing user pool
+}
+
+
+
+resource "aws_s3_bucket" "nook_bucket_dev" {
+  bucket = "nook-bucket-dev" # Replace with your unique bucket name
+  force_destroy = true
+
+  tags = {
+    Name        = "nook-bucket-dev"
+    Environment = "Dev"
+  }
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "nook_bucket_ownership" {
+  bucket = aws_s3_bucket.nook_bucket_dev.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.nook_bucket_dev.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+
+resource "aws_s3_bucket_acl" "nook_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.nook_bucket_ownership]
+
+  bucket = aws_s3_bucket.nook_bucket_dev.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_policy" "public_bucket_policy" {
+  bucket = aws_s3_bucket.nook_bucket_dev.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.nook_bucket_dev.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user" "s3_full_access_user" {
+  name = "s3-full-access-user"
+}
+
+resource "aws_iam_user_policy_attachment" "s3_full_access_policy" {
+  user       = aws_iam_user.s3_full_access_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_access_key" "s3_user_access_key" {
+  user = aws_iam_user.s3_full_access_user.name
+}
+
+output "aws_access_key_id" {
+  value = aws_iam_access_key.s3_user_access_key.id
+}
+
+output "aws_secret_access_key" {
+  value = aws_iam_access_key.s3_user_access_key.secret
+  sensitive = true
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.nook_bucket_dev.id
 }
